53ba4a0916128c9edb49960bb230471385605c4c
Ricardo Rocker <76121782+ricardorocker@users.noreply.github.com>
2023-10-28 11:25:14 -0300

    loan-calculator layoult and create reactive form

--- src/app/views/loan-calculator/loan-calculator.component.ts
+++ src/app/views/loan-calculator/loan-calculator.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-loan-calculator',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./loan-calculator.component.css']
 })
 export class LoanCalculatorComponent {
+  emprestimoForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.emprestimoForm = this.fb.group({
+      tipoEmprestimo: this.fb.group({
+        casa: [false],
+        pessoal: [false],
+        carro: [false],
+      }),
+      valorEmprestimo: [1000, [Validators.min(1000), Validators.max(50000)]],
+      taxaEmprestimo: [0, [Validators.min(0), Validators.max(25)]],
+      prazoEmprestimo: this.fb.group({
+        quantidade: [1, [Validators.min(1), Validators.max(60)]],
+        tipo: ['year'],
+      }),
+    });
+  }
+
+  onSubmit() {
+    const formData = this.emprestimoForm.value;
+    console.log(formData);
+  }
 }
