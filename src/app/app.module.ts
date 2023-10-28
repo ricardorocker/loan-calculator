78df1a40672fb716af05704a2ede5413a7753400
Ricardo Rocker <76121782+ricardorocker@users.noreply.github.com>
2023-10-27 22:28:56 -0300

    Navbar creation layout and implement routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './views/home/home.component';
+import { LoanCalculatorComponent } from './views/loan-calculator/loan-calculator.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LoanCalculatorComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/views/loan-calculator/loan-calculator.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loan-calculator',
+  templateUrl: './loan-calculator.component.html',
+  styleUrls: ['./loan-calculator.component.css']
+})
+export class LoanCalculatorComponent {
+
+}
