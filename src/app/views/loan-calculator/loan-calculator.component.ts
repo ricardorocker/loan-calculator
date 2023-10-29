cdd875cd084734fdcc362b36db72b2263d7f7e70
Ricardo Rocker <76121782+ricardorocker@users.noreply.github.com>
2023-10-29 16:32:35 -0300

    Radio input creation to reactive Form

--- src/app/views/loan-calculator/loan-calculator.component.ts
+++ src/app/views/loan-calculator/loan-calculator.component.ts
@@ -4,24 +4,31 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-loan-calculator',
   templateUrl: './loan-calculator.component.html',
-  styleUrls: ['./loan-calculator.component.css']
+  styleUrls: ['./loan-calculator.component.css'],
 })
 export class LoanCalculatorComponent {
-  emprestimoForm: FormGroup;
+  emprestimoForm!: FormGroup;
+  loanTypes = [
+    { id: 1, description: 'Casa' },
+    { id: 2, description: 'Carro' },
+    { id: 3, description: 'Pessoal' },
+  ];
 
-  constructor(private fb: FormBuilder) {
-    this.emprestimoForm = this.fb.group({
-      tipoEmprestimo: this.fb.group({
-        casa: [false],
-        pessoal: [false],
-        carro: [false],
-      }),
+  constructor(private form: FormBuilder) {
+    this.emprestimoForm = this.form.group({
+      loanType: [null, Validators.required],
       valorEmprestimo: [1000, [Validators.min(1000), Validators.max(50000)]],
       taxaEmprestimo: [0, [Validators.min(0), Validators.max(25)]],
-      prazoEmprestimo: this.fb.group({
-        quantidade: [1, [Validators.min(1), Validators.max(60)]],
-        tipo: ['year'],
-      }),
+      prazoEmprestimo: [1, [Validators.min(1), Validators.max(60)]],
+      tipoPrazo: ['', [Validators.min(1), Validators.max(60)]]
+    });
+  }
+
+  ngOnInit() {
+    // Inicializa o formulário com valores padrão se necessário
+    this.emprestimoForm.patchValue({
+      valorEmprestimo: 1000,
+      loanType: null,
     });
   }
 
