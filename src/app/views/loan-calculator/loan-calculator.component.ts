c5b1034dcad104358fbb86002a70371b5f3dc270
Ricardo Rocker <76121782+ricardorocker@users.noreply.github.com>
2023-11-01 21:14:51 -0300

    Form validations

--- src/app/views/loan-calculator/loan-calculator.component.ts
+++ src/app/views/loan-calculator/loan-calculator.component.ts
@@ -9,28 +9,27 @@ import { LoanCalculatorService } from 'src/app/services/loan-calculator.service'
   styleUrls: ['./loan-calculator.component.css'],
 })
 export class LoanCalculatorComponent {
-  loanForm!: FormGroup;
+  loanForm: FormGroup;
   loanTypes = [
     { id: 1, description: 'Home' },
     { id: 2, description: 'Car' },
     { id: 3, description: 'Personal' },
   ];
   graphData: Array<Graph> = [
-    {value: 1000, color:'#ff6b92', size:'', legend:'Principal'},
-    {value: 100, color:'#e4002b', size:'', legend:'Interest'},
+    { value: 1000, color: '#ff6b92', size: '', legend: 'Principal' },
+    { value: 100, color: '#e4002b', size: '', legend: 'Interest' },
   ];
 
   constructor(
-    private form: FormBuilder,
+    private formBuilder: FormBuilder,
     private calculatorService: LoanCalculatorService
   ) {
-    this.loanForm = this.form.group({
+    this.loanForm = this.formBuilder.group({
       loanType: [null, Validators.required],
-      loanAmount: [1000, [Validators.min(1000), Validators.max(50000)]],
-      interestAmount: [0, [Validators.min(0), Validators.max(25)]],
-      loanTerm: [1, [Validators.min(1), Validators.max(60)]],
-      termType: ['year', [Validators.min(1), Validators.max(60)]],      
-      
+      loanAmount: [1000, [Validators.required, Validators.min(1000), Validators.max(50000)]],
+      interestAmount: [10, [Validators.required, Validators.min(0.1), Validators.max(25)]],
+      loanTerm: [1, [Validators.required, Validators.min(1), Validators.max(60)]],
+      termType: ['year', Validators.required],
       loanTypeResponse: [''],
       principal: [1000],
       interestPayable: [100],
@@ -39,27 +38,33 @@ export class LoanCalculatorComponent {
   }
 
   onSubmit() {
-    const formData = this.loanForm.value;
-    this.calculatorService.simulateLoan(formData).subscribe(
-      (result) => {
-        this.loanForm.get('loanTypeResponse')?.setValue(result.loanTypeResponse);
-        this.loanForm.get('principal')?.setValue(result.principal);
-        this.loanForm.get('interestPayable')?.setValue(result.interestPayable);
-        this.loanForm.get('totalAmountPayable')?.setValue(result.totalAmountPayable);
+    console.log(this.loanForm)
+    if (this.loanForm.valid) {
+      const formData = this.loanForm.value;
+      this.calculatorService.simulateLoan(formData).subscribe(
+        (result) => {
+          this.loanForm.get('loanTypeResponse')!.setValue(result.loanTypeResponse);
+          this.loanForm.get('principal')!.setValue(result.principal);
+          this.loanForm.get('interestPayable')!.setValue(result.interestPayable);
+          this.loanForm.get('totalAmountPayable')!.setValue(result.totalAmountPayable);
 
-        const newGraphData = this.graphData.map((item, index) => {
-          if (index === 0) {
-            item.value = result.principal;
-          } else if (index === 1) {
-            item.value = result.interestPayable;
-          }
-          return item;
-        });
-        this.graphData = newGraphData;
-      },
-      (error) => {
-        console.error('Error to calculate', error);
-      }
-    );
+          const newGraphData = this.graphData.map((item, index) => {
+            if (index === 0) {
+              item.value = result.principal;
+            } else if (index === 1) {
+              item.value = result.interestPayable;
+            }
+            return item;
+          });
+          this.graphData = newGraphData;
+        },
+        (error) => {
+          console.error('Error calculating loan', error);
+        }
+      );
+    } else {
+      // Mark the form fields as touched to display validation errors
+      this.loanForm.markAllAsTouched();
+    }
   }
 }
