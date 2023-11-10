3a6775e1447406c6c14121cf00a0cf48fe22de24
Ricardo Rocker <76121782+ricardorocker@users.noreply.github.com>
2023-11-10 08:50:26 -0300

    ngOnInit, updateGraphData and updateFormValues to update loan-calculator more clean

--- src/app/views/loan-calculator/loan-calculator.component.ts
+++ src/app/views/loan-calculator/loan-calculator.component.ts
@@ -9,26 +9,46 @@ import { LoanCalculatorService } from 'src/app/services/loan-calculator.service'
   styleUrls: ['./loan-calculator.component.css'],
 })
 export class LoanCalculatorComponent {
-  loanForm: FormGroup;
+  loanForm!: FormGroup;
   loanTypes = [
     { id: 1, description: 'Home' },
     { id: 2, description: 'Car' },
     { id: 3, description: 'Personal' },
   ];
   graphData: Array<Graph> = [
-    { value: 1000, color: 'var(--secondary-color)', size: '', legend: 'Principal' },
+    {
+      value: 1000,
+      color: 'var(--secondary-color)',
+      size: '',
+      legend: 'Principal',
+    },
     { value: 100, color: 'var(--primary-color)', size: '', legend: 'Interest' },
   ];
 
   constructor(
     private formBuilder: FormBuilder,
     private calculatorService: LoanCalculatorService
-  ) {
+  ) {}
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  private initForm(): void {
     this.loanForm = this.formBuilder.group({
       loanType: [null, Validators.required],
-      loanAmount: [1000, [Validators.required, Validators.min(1000), Validators.max(200000)]],
-      interestAmount: [10, [Validators.required, Validators.min(0.1), Validators.max(25)]],
-      loanTerm: [1, [Validators.required, Validators.min(1), Validators.max(60)]],
+      loanAmount: [
+        1000,
+        [Validators.required, Validators.min(1000), Validators.max(200000)],
+      ],
+      interestAmount: [
+        10,
+        [Validators.required, Validators.min(0.1), Validators.max(25)],
+      ],
+      loanTerm: [
+        1,
+        [Validators.required, Validators.min(1), Validators.max(60)],
+      ],
       termType: ['year', Validators.required],
       loanTypeResponse: [''],
       principal: [1000],
@@ -38,34 +58,41 @@ export class LoanCalculatorComponent {
   }
 
   onSubmit() {
-    console.log(this.loanForm)
     if (this.loanForm.valid) {
-      console.log(this.loanForm)
-      const formData = this.loanForm.value;
-      this.calculatorService.simulateLoan(formData).subscribe(
-        (result) => {
-          this.loanForm.get('loanTypeResponse')!.setValue(result.loanTypeResponse);
-          this.loanForm.get('principal')!.setValue(result.principal);
-          this.loanForm.get('interestPayable')!.setValue(result.interestPayable);
-          this.loanForm.get('totalAmountPayable')!.setValue(result.totalAmountPayable);
+      console.log(this.loanForm);
 
-          const newGraphData = this.graphData.map((item, index) => {
-            if (index === 0) {
-              item.value = result.principal;
-            } else if (index === 1) {
-              item.value = result.interestPayable;
-            }
-            return item;
-          });
-          this.graphData = newGraphData;
+      const formData = this.loanForm.value;
+      this.calculatorService.simulateLoan(formData).subscribe({
+        next: (result) => {
+          this.updateGraphData(result);
+          this.updateFormValues(result);
         },
-        (error) => {
+        error: (error) => {
           console.error('Error calculating loan', error);
-        }
-      );
+        },
+      });
     } else {
-      // Mark the form fields as touched to display validation errors
       this.loanForm.markAllAsTouched();
     }
+  }
+
+  private updateGraphData(result: any): void {
+    this.graphData = this.graphData.map((item, index) => {
+      if (index === 0) {
+        item.value = result.principal;
+      } else if (index === 1) {
+        item.value = result.interestPayable;
+      }
+      return item;
+    });
+  }
+
+  private updateFormValues(result: any): void {
+    this.loanForm.patchValue({
+      loanTypeResponse: result.loanTypeResponse,
+      principal: result.principal,
+      interestPayable: result.interestPayable,
+      totalAmountPayable: result.totalAmountPayable,
+    });
   }
 }
